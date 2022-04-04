9d34b251b9dce94ad2d77e1b86ed0bf6cae22a34
Jason Maes <jason.maes@Jasons-MacBook-Pro.local>
2022-04-03 18:41:31 -0700

    add AWS cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_o3G7iR5vv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6jrru4o5sng88mqsfn5uego8i1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
